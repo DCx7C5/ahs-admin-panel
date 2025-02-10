66e5ffe75cd5dc36e3704e1a02a74e70878d1246
dcx7c5 <dcxdevelopment@protonmail.com>
2025-02-10 16:11:21 +0100

    Refactor ECC utils and initialization logic.
    Moved utility methods for ECC settings validation and model retrieval to `utils.py` for better organization. Simplified `__init__.py` by delegating checks to the ECC module, improving modularity and maintainability.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_crypto/__init__.py
+++ backend/ahs_crypto/__init__.py
@@ -1,63 +1,13 @@
 import importlib
-
+import logging
 from django.core.exceptions import ImproperlyConfigured
-from django.apps import apps as django_apps
-
-from .settings import validate_settings, ECC_BACKEND
-
-validate_settings()
-
-def get_ecc_backend():
-    """
-    Dynamically import the ECC backend class specified by the ECC_BACKEND setting
-    and return an instance of that class.
-    """
-
-    module_path, class_name = ECC_BACKEND.rsplit(".", 1)
-
-    try:
-        module = importlib.import_module(module_path)
-
-        ecc_backend_class = getattr(module, class_name)
-
-        return ecc_backend_class
-    except (ImportError, AttributeError) as e:
-        raise ImportError(f"Error importing '{ECC_BACKEND}': {e}")
+from .settings import ECC_BACKEND
+from .ecc import ECC
 
 
-def get_public_key_model():
-    try:
-        return django_apps.get_model(
-            settings.ECC_PRIVKEY_MODEL.replace("PrivateKey", "PublicKey"),
-            require_ready=False,
-        )
+logger = logging.getLogger(__name__)
 
-    except ValueError:
-        raise ImproperlyConfigured(
-            "ECC_PUBKEY_MODEL must be of the form 'app_label.model_name'"
-        )
-    except LookupError:
-        raise ImproperlyConfigured(
-            "ECC_PUBKEY_MODEL refers to model '%s' that has not been installed"
-            % settings.ECC_PRIVKEY_MODEL
-        )
+if not ECC.check_min_settings():
+    raise ImproperlyConfigured("ECC Crypto App is not configured correctly")
 
-def get_private_key_model():
-    """
-    Return the asymmetric key models (PrivateKey and PublicKey) used in this project.
-    """
-    try:
-        return django_apps.get_model(
-            settings.ECC_PRIVKEY_MODEL,
-            require_ready=False,
-        )
 
-    except ValueError:
-        raise ImproperlyConfigured(
-            "ECC_PRIVKEY_MODEL must be of the form 'app_label.model_name'"
-        )
-    except LookupError:
-        raise ImproperlyConfigured(
-            "ECC_PRIVKEY_MODEL refers to model '%s' that has not been installed"
-            % settings.ECC_PRIVKEY_MODEL
-        )
--- backend/ahs_crypto/utils.py
+++ backend/ahs_crypto/utils.py
@@ -1,12 +1,63 @@
-from backend.ahs_crypto import settings, get_ecc_backend
+import importlib
+from django.apps import apps as django_apps
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
 
-ECC_BACKEND_CLASS_STR: str = settings.ECC_BACKEND
 
-
-def validate_settings():
+def get_ecc_backend():
+    module_path, class_name = settings.ECC_BACKEND.rsplit(".", 1)
     try:
-        ECC = get_ecc_backend()
-        ECC.check_settings()
-        return True
-    except Exception as e:
-        raise Exception(f"Error validating settings: {e}")
+        module = importlib.import_module(module_path)
+
+        ecc_backend_class = getattr(module, class_name)
+
+        return ecc_backend_class
+    except (ImportError, AttributeError) as ex:
+        raise ImportError(f"Error importing '{settings.ECC_BACKEND}': {ex}")
+
+
+def validate_backend(backend_string: str) -> bool:
+    module_path, class_name = backend_string.rsplit(".", 1)
+    try:
+        module = importlib.import_module(module_path)
+        return hasattr(module, class_name)
+    except (ImportError, AttributeError) as ex:
+        raise ImportError(f"Error importing '{settings.ECC_BACKEND}': {ex}")
+
+
+def get_public_key_model():
+    try:
+        return django_apps.get_model(
+            settings.ECC_PRIVKEY_MODEL.replace("PrivateKey", "PublicKey"),
+            require_ready=False,
+        )
+
+    except ValueError:
+        raise ImproperlyConfigured(
+            "ECC_PUBKEY_MODEL must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            "ECC_PUBKEY_MODEL refers to model '%s' that has not been installed"
+            % settings.ECC_PRIVKEY_MODEL
+        )
+
+def get_private_key_model():
+    """
+    Return the asymmetric key models (PrivateKey and PublicKey) used in this project.
+    """
+    try:
+        return django_apps.get_model(
+            settings.ECC_PRIVKEY_MODEL,
+            require_ready=False,
+        )
+
+    except ValueError:
+        raise ImproperlyConfigured(
+            "ECC_PRIVKEY_MODEL must be of the form 'app_label.model_name'"
+        )
+    except LookupError:
+        raise ImproperlyConfigured(
+            "ECC_PRIVKEY_MODEL refers to model '%s' that has not been installed"
+            % settings.ECC_PRIVKEY_MODEL
+        )
