65f44507cc14d469118780da2057bd46bcd427fe
dcx7c5 <dcxdevelopment@protonmail.com>
2025-02-11 16:23:03 +0100

    Rename and reorganize "apps" and "ahs_channels".
    Renamed files and updated references to improve clarity and structure.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_channels/apps.py
+++ backend/ahs_channels/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AppsConfig(AppConfig):
+class AhsChannelsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'backend.apps'
+    name = 'backend.ahs_channels'
