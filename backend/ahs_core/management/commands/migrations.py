e9df0648575fb84c69ccf217436adaa8161d3d76
dcx7c5 <dcxdevelopment@protonmail.com>
2025-02-14 22:47:21 +0100

    Simplify migration command and remove unused import.
    Streamlined the migration command by updating the help text and removing the `sleep` delay. Eliminated an unnecessary import to improve code clarity and maintainability.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/management/commands/migrations.py
+++ backend/ahs_core/management/commands/migrations.py
@@ -1,15 +1,13 @@
-from time import sleep
 from django.core.management import BaseCommand
 from django.core.management.commands.makemigrations import Command as MakeMigrationsCommand
 from django.core.management.commands.migrate import Command as MigrateCommand
 from backend.ahs_core.management.commands.populate import Command as PopulateCommand
 from backend.ahs_core.management.commands.cleanmigrations import Command as CleanMigrationsCommand
-from backend.ahs_core.utils import get_all_core_apps, get_all_plugin_apps
-
+from backend.ahs_core.utils import get_all_core_apps, get_all_plugin_apps, Docker
 
 
 class Command(BaseCommand):
-    help = "Executes makemigrations and migrate commands to clean up migration directories and contents."
+    help = "Shortcut command that executes makemigrations and migrate commands."
 
     def add_arguments(self, parser):
 
@@ -44,7 +42,6 @@ class Command(BaseCommand):
         MakeMigrationsCommand().run_from_argv(['manage.py', 'makemigrations'] + all_apps)
         self.stdout.write(self.style.SUCCESS('Creating migrations... Done'))
         self.stdout.write(self.style.WARNING('Migrating...'))
-        sleep(2)
         MigrateCommand().run_from_argv(['manage.py', 'migrate'])
         self.stdout.write(self.style.SUCCESS('Migrating... Done'))
         if options['populate']:
