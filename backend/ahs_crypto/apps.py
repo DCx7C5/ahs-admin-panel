bde02bfd7167ea2614140d1f631f083c2c3cb008
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-03 02:30:05 +0100

    Rename app config class and add logging setup
    Renamed `AhsCryptoConfig` to `CryptoTokenConfig` for clarity and updated `verbose_name` for better description. Introduced a logger instance to the app configuration for potential logging needs.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_crypto/apps.py
+++ backend/ahs_crypto/apps.py
@@ -1,6 +1,13 @@
+import logging
+
 from django.apps import AppConfig
 
 
-class AhsCryptoConfig(AppConfig):
+
+logger = logging.getLogger(__name__)
+
+
+class CryptoTokenConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backend.ahs_crypto'
+    verbose_name = 'Django Cryptography Backend'
