3c8f7e3ff62d244127aebbc6d1a2f3fd4cc2e75b
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-03 17:54:50 +0100

    Refactor routing and add authentication wrappers.
    Updated `Root` component to include an `AuthProvider` and protect routes using `AuthProtectedRoutes`. Introduced lazy-loaded components for `Login`, `Test`, and `Settings` pages, and adjusted imports for improved structure and clarity.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- frontend/js/Root.tsx
+++ frontend/js/Root.tsx
@@ -1,46 +1,55 @@
 import React, {lazy, Suspense} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Layout from "./Layout";
+import Layout from "./components/Layout/Layout";
+import AuthProtectedRoutes from "./components/AuthProtectedRoutes";
+import AuthProvider from "./components/AuthProvider";
 
 
-const DashBoard = lazy(() => import("./pages/dashboard/Dashboard")
+const DashBoard = lazy(() => import("./pages/Dashboard")
     .then(module => ({
-        default: module.Dashboard
+        default: module.Dashboard,
+    }))
+);
+
+const Login = lazy(() => import("./pages/Login")
+    .then(module => ({
+        default: module.Login,
+    }))
+);
+
+const Test = lazy(() => import("./pages/Test")
+    .then(module => ({
+        default: module.Test,
+    }))
+);
+
+const Settings = lazy(() => import("./pages/Settings")
+    .then(module => ({
+        default: module.Settings,
     }))
 );
 
 
-const Test = () => {
-    return (
-        <div>
-            <h1>Test Page</h1>            
-            <h1>Test Page</h1>
-            <h1>Test Page</h1>
-            <h1>Test Page</h1>
-
-        </div>
-    )
-}
-
-
 export const Root = () => {
-
-    return (
-        <Suspense>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />} >
-                        <Route index element={<DashBoard />} />
-                        <Route path='dashboard' element={<DashBoard />} />
-                        <Route path='xapi' element={<Test />} >
-                            <Route path='settings' element={<Test />} />
-                        </Route>
-                        <Route path='settings' element={<Test />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </Suspense>
-    )
-}
+  return (
+    <AuthProvider>
+      <Suspense>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route path="login" element={<Login />} />
+              <Route element={<AuthProtectedRoutes />}>
+                <Route index element={<DashBoard />} />
+                <Route path="dashboard" element={<DashBoard />} />
+                <Route path="settings" element={<Settings />} />
+                <Route path="logout" element={<Test />} />
+              </Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </AuthProvider>
+  );
+};
 
 export default Root;
