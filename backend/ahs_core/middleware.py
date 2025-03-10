b491a3ef9e878a7ac072f8f26443c6522e1f7577
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-10 10:21:31 +0100

    Add AHSAdminPanelMiddleware for session and auth management
    Introduce `AHSAdminPanelMiddleware` to handle sessions, authentication, and messages for the `/admin` backend. It ensures proper request and response processing, session persistence, and message handling, improving middleware functionality and maintainability.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/middleware.py
+++ backend/ahs_core/middleware.py
@@ -1,42 +1,129 @@
-from asgiref.sync import iscoroutinefunction, markcoroutinefunction
-from django.contrib.auth import get_user_model
-from django.core.handlers.asgi import ASGIRequest
-from django.contrib.auth import aget_user
+import logging
+import time
+from functools import partial
+from importlib import import_module
 
-from backend.ahs_accounts.serializers import AHSUserSerializer
-from backend.ahs_endpoints.models import EndPoint
-from backend.ahs_endpoints.serializers import EndPointSerializer
+from asgiref.sync import sync_to_async
+from django.conf import settings
+from django.contrib.auth import get_user_model, get_user
+from django.contrib.auth.middleware import auser
+from django.contrib.messages.storage import default_storage
+from django.contrib.sessions.backends.base import UpdateError
+from django.contrib.sessions.exceptions import SessionInterrupted
+from django.http import HttpRequest, HttpResponse
+from django.utils.cache import patch_vary_headers
+from django.utils.deprecation import MiddlewareMixin
+from django.utils.functional import SimpleLazyObject
+from django.utils.http import http_date
+
+logger = logging.getLogger(__name__)
 
 User = get_user_model()
 
 
-class AHSMiddleware:
+class AHSAdminPanelMiddleware(MiddlewareMixin):
+    """
+    Manages sessions, session authentication and messages for the /admin backend
+    """
     async_capable = True
     sync_capable = False
 
     def __init__(self, get_response):
-        self.get_response = get_response
-        if iscoroutinefunction(self.get_response):
-            markcoroutinefunction(self)
+        super().__init__(get_response)
+        engine = import_module(settings.SESSION_ENGINE)
+        self.SessionStore = engine.SessionStore
 
-    async def __call__(self, request: ASGIRequest):
-        return await self.get_response(request)
-
-    async def process_template_response(self, request, response):
-        if not hasattr(request, '_cached_user'):
-            request._cached_user = await aget_user(request)
-
-        user = request._cached_user  # noqa
-        if user.is_authenticated:
-            response.context_data["AHS_USER"] = await AHSUserSerializer(user).adata
-
-        await self.get_pages_data(response)
+    async def __call__(self, request: HttpRequest):
+        if not request.path.startswith('/admin/'):
+            return await self.get_response(request)
+        await self.process_request(request)
+        response = await self.get_response(request)
+        response = await self.process_response(request, response)
         return response
 
-    @staticmethod
-    async def get_pages_data(response):
-        response.context_data["PAGES"] = []
-        async for item in EndPoint.objects.all():
-            response.context_data["PAGES"].append(await EndPointSerializer(item).adata)
+    async def process_request(self, request: HttpRequest) -> None:
+        # session
+        logger.debug("process_request session")
+        session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
+        request.session = self.SessionStore(session_key)
+        # auth
+        logger.debug("process_request auth")
 
+        request.user = SimpleLazyObject(lambda: get_user(request))  # noqa
+        request.auser = partial(auser, request)
+        # messages
+        logger.debug("process_request messages")
+        request._messages = await sync_to_async(default_storage)(request)
+
+
+    async def process_response(self, request, response) -> HttpResponse:
+        # Messages
+        logger.debug("process_response messages")
+
+        if hasattr(request, "_messages"):
+            unstored_messages = await sync_to_async(request._messages.update)(response)  # noqa
+            if unstored_messages and settings.DEBUG:
+                raise ValueError("Not all temporary messages could be stored.")
+        # Session
+        logger.debug("process_response Session")
+
+        try:
+            accessed = request.session.accessed
+            modified = request.session.modified
+            empty = request.session.is_empty()
+        except AttributeError:
+            return response
+        # First check if we need to delete this cookie.
+        # The session should be deleted only if the session is entirely empty.
+        if settings.SESSION_COOKIE_NAME in request.COOKIES and empty:
+            response.delete_cookie(
+                settings.SESSION_COOKIE_NAME,
+                path=settings.SESSION_COOKIE_PATH,
+                domain=settings.SESSION_COOKIE_DOMAIN,
+                samesite=settings.SESSION_COOKIE_SAMESITE,
+            )
+            patch_vary_headers(response, ("Cookie",))
+        else:
+            if accessed:
+                patch_vary_headers(response, ("Cookie",))
+            if (modified or settings.SESSION_SAVE_EVERY_REQUEST) and not empty:
+                if await request.session.aget_expire_at_browser_close():
+                    max_age = None
+                    expires = None
+                else:
+                    max_age = await request.session.aget_expiry_age()
+                    expires_time = time.time() + max_age
+                    expires = http_date(expires_time)
+                # Save the session data and refresh the client cookie.
+                # Skip session save for 5xx responses.
+                if response.status_code < 500:
+                    try:
+                        await request.session.asave()
+                    except UpdateError:
+                        raise SessionInterrupted(
+                            "The request's session was deleted before the "
+                            "request completed. The user may have logged "
+                            "out in a concurrent request, for example."
+                        )
+                    response.set_cookie(
+                        settings.SESSION_COOKIE_NAME,
+                        request.session.session_key,
+                        max_age=max_age,
+                        expires=expires,
+                        domain=settings.SESSION_COOKIE_DOMAIN,
+                        path=settings.SESSION_COOKIE_PATH,
+                        secure=settings.SESSION_COOKIE_SECURE or None,
+                        httponly=settings.SESSION_COOKIE_HTTPONLY or None,
+                        samesite=settings.SESSION_COOKIE_SAMESITE,
+                    )
+        return response
+
+
+class AHSSessionTokenMiddleware(MiddlewareMixin):
+    async_capable = True
+    sync_capable = False
+
+    def __init__(self, get_response):
+        super().__init__(get_response)
+        engine = import_module(settings.SESSION_ENGINE)
 
