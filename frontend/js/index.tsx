0397534746c615df16ae79166d6122bb15c8d7ae
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-10 10:05:49 +0100

    Refactor root element assignment in index.tsx
    Simplify the code by directly passing the `getElementById` call to `ReactDOM.createRoot`. This improves readability and removes an unnecessary variable declaration.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- frontend/js/index.tsx
+++ frontend/js/index.tsx
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom/client";
 import Root from "./Root";
 import "../css/base.scss"
 
-
-const rootElement = document.getElementById("root");
-ReactDOM.createRoot(rootElement!).render(<Root />);
+ReactDOM.createRoot(document.getElementById("root")!).render(<Root />);
