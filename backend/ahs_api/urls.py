5c40622868d160c1ec0b2207bd87080dff79952a
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-10 10:20:58 +0100

    Add custom async authentication and replace token routes
    Introduces `AsyncAuthentication` and two new async views: `SignupView` and `LoginView`. Updates URL configuration to replace previous token-related endpoints with `/login` and `/signup`.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_api/urls.py
+++ backend/ahs_api/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView, TokenBlacklistView,
-)
 
-from backend.ahs_api.serializers import AHSTokenObtainPairSerializer
+from backend.ahs_api.views import LoginView, SignupView
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(
-        serializer_class=AHSTokenObtainPairSerializer), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('signup/', SignupView.as_view(), name='signup'),
 ]
