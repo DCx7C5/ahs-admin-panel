cccabcd92c5ffcf76ef48e37fb2f643e49058db6
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-10 10:08:33 +0100

    Add AuthProtectedRoutes component for route protection
    Introduce a new component that restricts access to certain routes based on authentication status. Redirects users to the login page if they are not authenticated, ensuring secure route handling.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

---
+++ frontend/js/components/AuthProtectedRoutes.tsx
@@ -0,0 +1,18 @@
+import React, {use, useEffect} from "react";
+import {Outlet, useNavigate} from "react-router-dom";
+import {DataContext} from "./DataProvider";
+
+const AuthProtectedRoutes = () => {
+  const {isAuthenticated} = use(DataContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate("login/", { replace: true });
+    }
+  }, []);
+
+  return <Outlet />;
+};
+
+export default AuthProtectedRoutes;
