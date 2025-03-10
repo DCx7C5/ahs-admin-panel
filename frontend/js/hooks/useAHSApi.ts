a07c8f40e4eb1bf2a1f3fdea8562388a7cb552f7
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-10 10:06:12 +0100

    Refactor useAHSApi hook to TypeScript with enhanced structure
    Rewrote the useAHSApi hook in TypeScript for better type safety and readability. Added improved API methods (GET and POST) with a cleaner structure and centralized axios configuration. This refactor improves maintainability and aligns with TypeScript standards.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

---
+++ frontend/js/hooks/useAHSApi.ts
@@ -0,0 +1,70 @@
+import axios, {AxiosResponse} from "axios";
+import {useCallback, useState} from "react";
+
+
+interface AHSApi {
+  get: (endpoint: string, requestData?: any, config?: any) => Promise<any>;
+  post: (endpoint: string, requestData?: any, config?: any) => Promise<any>;
+  isLoading: boolean;
+  error: string;
+  data: any;
+}
+
+
+
+export const useAHSApi = (): AHSApi => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState({});
+
+  const api = axios.create({
+    baseURL: `${window.location.origin}/`,
+    httpsAgent: false,
+
+  });
+
+  const request = useCallback(
+    async (endpoint, method="POST", requestData={}, config={}): Promise<AxiosResponse> => {
+      setIsLoading(true);
+      setError(null);
+
+      try {
+        const cfg = {endpoint, method, requestData, ...config,}
+        const response = await api.request(cfg);
+
+        setData((prev) => ({ ...prev, [endpoint]: response.data }));
+        return response.data;
+      } catch (err) {
+        setError(err?.response?.data?.message || err.message || "Something went wrong");
+        throw err;
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    []
+  );
+
+  const get = useCallback(
+    async (endpoint: string, requestData: any = {}, config: any = {}): Promise<AxiosResponse> => {
+      const response = await request(endpoint, 'GET', config);
+      return response.data;
+    }, [request]
+  );
+
+  const post = useCallback(
+    async (endpoint: string, requestData: any = {}, config: any = {}): Promise<AxiosResponse> => {
+      const response = await request(endpoint, 'POST', config);
+      return response.data;
+    }, [request]
+  );
+
+  return {
+    get,
+    post,
+    isLoading,
+    error,
+    data,
+  };
+};
+
+export default useAHSApi;
