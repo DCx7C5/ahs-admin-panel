5662d1e7373cc858603bf2bff7d762b197a14e7e
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-22 23:22:07 +0100

    Refactor Sidebar component to simplify context usage
    Replaced DataContext with AuthContext to fetch user data. Removed unused imports and SidebarToggleButton for cleaner and more efficient code. Ensured functionality remains consistent while reducing complexity.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- frontend/js/components/Sidebar/Sidebar.jsx
+++ frontend/js/components/Sidebar/Sidebar.jsx
@@ -1,29 +1,23 @@
 import React, {use, useRef} from 'react';
-import {createPortal} from "react-dom";
-import {DataContext} from "../DataProvider";
 import SidebarFooter from "./SidebarFooter";
 import SidebarProfile from "./SidebarProfile";
 import SidebarMenu from "./SidebarMenu";
-import SidebarToggleButton from "./SidebarToggleButton";
 import useHideElement from "../../hooks/useHideElement";
 import SidebarHeader from "./SidebarHeader";
 import SidebarBackdrop from "./SidebarBackdrop";
+import {AuthContext} from "../AuthProvider";
 import './sidebar.scss';
 
 
-export const Sidebar = ({toggleClass}) => {
+export const Sidebar = () => {
   const sidebarRef = useRef(null);
-  const { username, image } = use(DataContext).user;
+  const { username, image } = use(AuthContext);
   const [isVisible, hide,, toggleVisibility] = useHideElement(
     false,'sb.show', sidebarRef
   )
 
   return (
     <>
-      {createPortal(
-        <SidebarToggleButton onToggle={toggleVisibility} />,
-        document.getElementById(toggleClass)
-      )}
       {isVisible && <SidebarBackdrop onToggle={toggleVisibility} />}
       <div ref={sidebarRef} className='sidebar'>
         <SidebarHeader />
