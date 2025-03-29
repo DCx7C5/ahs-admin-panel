97c5065ba809ec83b3ce169ecf30627f0fc7205c
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-29 18:38:45 +0100

    Add token-related serializers to support authentication
    Introduce `TokenHeaderSerializer`, `TokenPayloadSerializer`, and `TokenSerializer` for token structure handling. These classes help define and validate token headers, payloads, and overall token details more efficiently.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/serializers.py
+++ backend/ahs_core/serializers.py
@@ -1,4 +1,5 @@
-from adrf.serializers import ModelSerializer
+from rest_framework.serializers import DateTimeField, IntegerField
+from adrf.serializers import ModelSerializer, Serializer
 from django.contrib.auth import get_user_model
 
 
@@ -13,3 +14,35 @@ class AHSUserSerializer(ModelSerializer):
             'email', 'is_active', 'is_staff', 'is_superuser',
             'last_login', 'image', 'uid',
         ]
+
+
+class TokenHeaderSerializer(Serializer):
+    token_id = IntegerField()
+    created = DateTimeField()
+    expired = DateTimeField()
+
+    class Meta:
+        fields = ['token_id', 'created', 'expired']
+
+
+class TokenPayloadSerializer(Serializer):
+    """
+    Serializer for the token payload
+    """
+
+    class Meta:
+        fields = ['user', 'session_key', 'session_token']
+
+
+
+class TokenSerializer(Serializer):
+    """
+    Serializer for the token
+    """
+
+    header = TokenHeaderSerializer()
+    payload = TokenPayloadSerializer()
+
+
+    class Meta:
+        fields = ['header', 'payload']
