00f300faa4cd1c89789a6a656764dcb7dfc1d7a7
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-29 18:39:02 +0100

    Refactor middleware to separate concerns and support token-based auth
    Reorganized middleware classes to isolate session, token, auth, and messages handling into distinct components. Introduced `AHSTokenMiddleware` for token parsing and `AHSAuthenticationMiddleware` for user authentication. Improved reusability and maintainability by implementing separate utility functions for user retrieval.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/middleware.py
+++ backend/ahs_core/middleware.py
@@ -5,8 +5,11 @@ from importlib import import_module
 
 from asgiref.sync import sync_to_async
 from django.conf import settings
-from django.contrib.auth import get_user_model, get_user
-from django.contrib.auth.middleware import auser
+from django.core.exceptions import ImproperlyConfigured
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import AnonymousUser
 from django.contrib.messages.storage import default_storage
 from django.contrib.sessions.backends.base import UpdateError
 from django.contrib.sessions.exceptions import SessionInterrupted
@@ -16,14 +19,33 @@ from django.utils.deprecation import MiddlewareMixin
 from django.utils.functional import SimpleLazyObject
 from django.utils.http import http_date
 
+from backend.ahs_core.auth import get_ahs_session_store, get_user_from_token_request, aget_user_from_token_request
+from backend.ahs_core.token import AHSToken
+
 logger = logging.getLogger(__name__)
 
 User = get_user_model()
+SessionStore = get_ahs_session_store()
+
+
+def get_cached_user(request: HttpRequest) -> AnonymousUser | AbstractBaseUser:
+    _cached_user = None
+    if not hasattr(request, "_cached_user"):
+        request._cached_user = get_user_from_token_request(request)
+    return _cached_user  # noqa
+
+
+async def get_acached_user(request: HttpRequest) -> AnonymousUser | AbstractBaseUser:
+    _acached_user = None
+    if not hasattr(request, "_acached_user"):
+        request._acached_user = await aget_user_from_token_request(request)
+    return _acached_user  # noqa
 
 
 class AHSAdminPanelMiddleware(MiddlewareMixin):
     """
-    Manages sessions, session authentication and messages for the /admin backend
+        Summarizes session, auth & messages middleware in one class and
+        restricts usage to admin panel only.
     """
     async_capable = True
     sync_capable = False
@@ -46,10 +68,11 @@ class AHSAdminPanelMiddleware(MiddlewareMixin):
         logger.debug("process_request session")
         session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME)
         request.session = self.SessionStore(session_key)
+        print(request.session)
         # auth
         logger.debug("process_request auth")
-        request.user = SimpleLazyObject(lambda: get_user(request))  # noqa
-        request.auser = partial(auser, request)
+        request.user = SimpleLazyObject(lambda: get_cached_user(request))  # noqa
+        request.auser = partial(get_acached_user, request)
         # messages
         logger.debug("process_request messages")
         request._messages = await sync_to_async(default_storage)(request)
@@ -118,78 +141,48 @@ class AHSAdminPanelMiddleware(MiddlewareMixin):
         return response
 
 
-class AHSSessionMiddleware(MiddlewareMixin):
+class AHSTokenMiddleware(MiddlewareMixin):
     async_capable = True
     sync_capable = False
 
     def __init__(self, get_response):
         super().__init__(get_response)
-        engine = import_module(settings.AHS_SESSION_ENGINE)
-        self.SessionStore = engine.SessionStore
+        self.TokenStore = import_module(settings.AHS_TOKEN_ENGINE).AHSTokenStore
 
     async def __call__(self, request: HttpRequest):
+        if request.path.startswith('/admin/'):
+            return await self.get_response(request)
         await self.process_request(request)
         response = await self.get_response(request)
         response = await self.process_response(request, response)
         return response
 
     async def process_request(self, request: HttpRequest) -> None:
-        session_token = request.COOKIES.get('token')
-        request.session = self.SessionStore(session_token)
-        logger.debug(f"process_request headers {request.headers}")
-        logger.debug(f"process_request cookies {request.COOKIES}")
-        logger.debug(f"process_request META {request.META}")
+        token = request.headers.get('X-AHS-Token')
+        request.token = AHSToken(token) if token else None
 
-    async def process_response(self, request, response) -> HttpResponse:
-        try:
-            accessed = request.session.accessed
-            modified = request.session.modified
-            empty = request.session.is_empty()
-        except AttributeError:
-            return response
-        logger.debug(f"process_response headers {response.headers}")
-        logger.debug(f"process_response cookies {response.cookies}")
-        # First check if we need to delete this cookie.
-        # The session should be deleted only if the session is entirely empty.
-        if settings.SESSION_COOKIE_NAME in request.COOKIES and empty:
-            response.delete_cookie(
-                settings.SESSION_COOKIE_NAME,
-                path=settings.SESSION_COOKIE_PATH,
-                domain=settings.SESSION_COOKIE_DOMAIN,
-                samesite=settings.SESSION_COOKIE_SAMESITE,
-            )
-            patch_vary_headers(response, ("Cookie",))
-        else:
-            if accessed:
-                patch_vary_headers(response, ("Cookie",))
-            if (modified or settings.SESSION_SAVE_EVERY_REQUEST) and not empty:
-                if await request.session.aget_expire_at_browser_close():
-                    max_age = None
-                    expires = None
-                else:
-                    max_age = await request.session.aget_expiry_age()
-                    expires_time = time.time() + max_age
-                    expires = http_date(expires_time)
-                # Save the session data and refresh the client cookie.
-                # Skip session save for 5xx responses.
-                if response.status_code < 500:
-                    try:
-                        await request.session.asave()
-                    except UpdateError:
-                        raise SessionInterrupted(
-                            "The request's session was deleted before the "
-                            "request completed. The user may have logged "
-                            "out in a concurrent request, for example."
-                        )
-                    response.set_cookie(
-                        settings.SESSION_COOKIE_NAME,
-                        request.session.session_key,
-                        max_age=max_age,
-                        expires=expires,
-                        domain=settings.SESSION_COOKIE_DOMAIN,
-                        path=settings.SESSION_COOKIE_PATH,
-                        secure=settings.SESSION_COOKIE_SECURE or None,
-                        httponly=settings.SESSION_COOKIE_HTTPONLY or None,
-                        samesite=settings.SESSION_COOKIE_SAMESITE,
-                    )
+    async def process_response(self, request, response):
+
         return response
+
+
+class AHSAuthenticationMiddleware(MiddlewareMixin):
+    async_capable = True
+    sync_capable = False
+
+    async def process_request(self, request: HttpRequest) -> None:
+        if not hasattr(request, "token"):
+            raise ImproperlyConfigured(
+                "The AHS authentication middleware requires token "
+                "middleware to be installed. Edit your MIDDLEWARE setting to "
+                "insert "
+                "'backend.ahs_core.middlewares.AHSTokenMiddleware' before "
+                "'backend.ahs_core.middlewares.AHSAuthenticationMiddleware'."
+            )
+        request.user = SimpleLazyObject(lambda: get_cached_user(request))  # noqa
+        request.auser = partial(get_acached_user, request)
+
+
+class AHSMessagesMiddleware(MiddlewareMixin):
+    async_capable = True
+    sync_capable = False
