4aebd7abb31d6e3c82eb7881ff0d6942cc6b9274
dcx7c5 <dcxdevelopment@protonmail.com>
2025-03-29 18:51:19 +0100

    Add custom AHSUserManager and public key handling to AHSUser
    Introduced a custom AHSUserManager to support user and superuser creation, with both synchronous and asynchronous methods. Added public key hashing and verification methods to AHSUser for enhanced security and flexibility. These changes improve user management and adhere to project-specific requirements.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/models/accounts.py
+++ backend/ahs_core/models/accounts.py
@@ -1,6 +1,7 @@
 import logging
 import uuid
-
+from django.apps import apps
+from django.contrib.auth.hashers import make_password, verify_password
 from django.db.models import ImageField
 from django.db.models.constraints import UniqueConstraint
 from django.db.models.fields import UUIDField, DateTimeField, CharField, URLField
@@ -9,12 +10,88 @@ from django.db.models.indexes import Index
 
 from django.urls import reverse
 from django.utils.translation import gettext as _
-from django.contrib.auth.models import AbstractUser, Permission
+from django.contrib.auth.models import AbstractUser, Permission, UserManager
 
+from backend.ahs_core.hashers import verify_publickey
 from backend.ahs_core.validators import AHSUsernameValidator
 
 logger = logging.getLogger(__name__)
 
+
+make_publickey = make_password
+
+
+class AHSUserManager(UserManager):
+    def _create_user_object(self, username, email, password, **extra_fields):
+        if not username:
+            raise ValueError("The given username must be set")
+        email = self.normalize_email(email)
+        # Lookup the real model class from the global app registry so this
+        # manager method can be used in migrations. This is fine because
+        # managers are by definition working on the real model.
+        GlobalUserModel = apps.get_model(
+            self.model._meta.app_label, self.model._meta.object_name
+        )
+        username = GlobalUserModel.normalize_username(username)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.password = make_password(password)
+        return user
+
+    def _create_user(self, username, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        user = self._create_user_object(username, email, password, **extra_fields)
+        user.save(using=self._db)
+        return user
+
+    async def _acreate_user(self, username, email, password, **extra_fields):
+        """See _create_user()"""
+        user = self._create_user_object(username, email, password, **extra_fields)
+        await user.asave(using=self._db)
+        return user
+
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(username, email, password, **extra_fields)
+
+    create_user.alters_data = True
+
+    async def acreate_user(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return await self._acreate_user(username, email, password, **extra_fields)
+
+    acreate_user.alters_data = True
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(username, email, password, **extra_fields)
+
+    create_superuser.alters_data = True
+
+    async def acreate_superuser(
+        self, username, email=None, password=None, **extra_fields
+    ):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return await self._acreate_user(username, email, password, **extra_fields)
+
+    acreate_superuser.alters_data = True
 
 
 
@@ -36,31 +113,69 @@ class AHSUser(AbstractUser):
             "unique": _("A user with that username already exists."),
         },
     )
+
     uid = UUIDField(
         unique=True,
         default=uuid.uuid4,
         editable=False,
         verbose_name=_('User UUID'),
     )
+
     image = ImageField(
         verbose_name=_('Profile Picture'),
         upload_to='pfps',
         default='pfps/default.jpg',
         blank=True,
     )
+
     date_modified = DateTimeField(
         verbose_name=_('Last Modified'),
         auto_now=True,
     )
+
     socket_url = URLField(
         verbose_name=_('Socket URL'),
     )
+
     public_key = CharField(
         verbose_name=_('Public Key'),
         blank=False,
         null=False,
     )
 
+    objects = AHSUserManager()
+
+    def set_publickey(self, public_key: str) -> None:
+        self.public_key = make_publickey(public_key)
+        self._public_key = public_key
+
+    def check_publickey(self, public_key):
+        is_correct, must_update = verify_publickey(
+            publickey=public_key,
+            encoded_publickey=self.public_key,
+            preferred="default",
+        )
+        if is_correct and must_update:
+            self.set_publickey(public_key)
+        return is_correct
+
+    async def acheck_publickey(self, public_key):
+        """See check_password()."""
+
+        async def setter(pk):
+            self.set_publickey(pk)
+            # Password hash upgrades shouldn't be considered password changes.
+            self._public_key = None
+            await self.asave(update_fields=["password"])
+
+        is_correct, must_update = verify_publickey(
+            publickey=public_key,
+            encoded_publickey=self.public_key,
+            preferred="default",
+        )
+        if setter and is_correct and must_update:
+            await setter(public_key)
+        return is_correct
 
     @property
     def permissions(self):
