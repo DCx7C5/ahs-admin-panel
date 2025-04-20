595efaa5f6d389a0fb40ecbb3ec880437d9c3d36
dcx7c5 <dcxdevelopment@protonmail.com>
2025-04-20 21:56:06 +0200

    Simplify AHSUserAdmin by removing email replacements
    - Removed logic replacing "email" with "public_key" in fieldsets, list_display, and search_fields.
    - Streamlined code for better readability and maintainability.
    - Retained all necessary functionality without altering core behavior.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_auth/admin.py
+++ backend/ahs_auth/admin.py
@@ -8,14 +8,7 @@ User = get_user_model()
 @admin.register(User)
 class AHSUserAdmin(UserAdmin):
     model = User
-    fieldsets = tuple(tuple(
-        (section[0], {"fields": tuple(field if field != "email" else "public_key" for field in section[1]["fields"])})
-        for
-        section in UserAdmin.fieldsets))
-
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('image',)}),
     )
-    list_display = tuple(field if field != "email" else "public_key" for field in UserAdmin.list_display)
-    search_fields = tuple(field if field != "email" else "public_key" for field in UserAdmin.search_fields)
     show_full_result_count = True
