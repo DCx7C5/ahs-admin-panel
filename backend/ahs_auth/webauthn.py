2ea6f0cbf2d1466a07a7abd9c2c9a31083efe768
dcx7c5 <dcxdevelopment@protonmail.com>
2025-04-23 22:18:39 +0200

    Update supported WebAuthn algorithms in ahs_auth.webauthn
    - Added ECDSA and RSASSA-PKCS1 algorithms for broader compatibility.
    - Adjusted the order of algorithms for better readability and logic.
    - Ensured alignment with WebAuthn specifications and allowed hosts.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_auth/webauthn.py
+++ backend/ahs_auth/webauthn.py
@@ -1,14 +1,15 @@
 from django.conf import settings
-from webauthn.helpers.cose import COSEAlgorithmIdentifier
 
+from webauthn.helpers.cose import COSEAlgorithmIdentifier
 
 
 RP_NAME = settings.SITE_NAME
 EXPECTED_RP_ID = settings.DOMAIN_NAME
 EXPECTED_ORIGIN = [f"https://{host}" for host in settings.ALLOWED_HOSTS]
 SUPPORTED_ALGOS = [
+    COSEAlgorithmIdentifier.EDDSA,
     COSEAlgorithmIdentifier.ECDSA_SHA_512,
     COSEAlgorithmIdentifier.ECDSA_SHA_256,
-    COSEAlgorithmIdentifier.EDDSA,
-    COSEAlgorithmIdentifier.RSASSA_PSS_SHA_512,
+    COSEAlgorithmIdentifier.RSASSA_PKCS1_v1_5_SHA_256,
+    COSEAlgorithmIdentifier.RSASSA_PKCS1_v1_5_SHA_512,
 ]
