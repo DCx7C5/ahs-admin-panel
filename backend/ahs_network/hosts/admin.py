b893820f04dd3afad767c0a670cd346e8c2f1baf
dcx7c5 <dcxdevelopment@protonmail.com>
2025-05-03 02:19:48 +0200

    Enhance HostAdmin with display, search, and filter options
    - Added `list_display` to show more fields in the admin list view.
    - Introduced `search_fields` to enable searching by hostname.
    - Included `list_filter` for filtering by system and localhost status.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_network/hosts/admin.py
+++ backend/ahs_network/hosts/admin.py
@@ -6,4 +6,7 @@ from backend.ahs_network.hosts.models import Host
 
 @admin.register(Host)
 class HostAdmin(ModelAdmin):
+    list_display = ('id', 'hostname', 'is_localhost', 'is_systemhost', 'created_at', 'updated_at', 'workspace')
+    search_fields = ('hostname',)
     ordering = ('id',)
+    list_filter = ('is_systemhost', 'is_localhost')
