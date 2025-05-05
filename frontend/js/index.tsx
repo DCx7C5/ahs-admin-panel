e7dde981eb58d525f77f16ee874cd15f57fa5854
dcx7c5 <dcxdevelopment@protonmail.com>
2025-05-06 00:04:07 +0200

    Remove DevSupport from React app initialization
    - Removed the DevSupport component along with related imports to simplify the ReactDOM initialization process.
    - This streamlines the production build by excluding development-specific dependencies.
    - The React app now directly renders the Root component within StrictMode.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- frontend/js/index.tsx
+++ frontend/js/index.tsx
@@ -2,17 +2,10 @@ import React, {StrictMode} from "react";
 import ReactDOM from "react-dom/client";
 import Root from "./Root";
 import "../css/base.scss"
-import {DevSupport} from "@react-buddy/ide-toolbox";
-import ComponentPreviews from "./dev/previews";
-import {useInitial} from "./dev";
 
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <DevSupport ComponentPreviews={ComponentPreviews}
-                    useInitialHook={useInitial}
-        >
-            <Root />
-        </DevSupport>
+        <Root />
     </StrictMode>
 );
