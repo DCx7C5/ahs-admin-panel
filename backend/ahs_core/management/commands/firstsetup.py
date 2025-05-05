7b410df08dfbf5d8541c6a6c150573a066a4d03f
dcx7c5 <dcxdevelopment@protonmail.com>
2025-05-05 17:06:12 +0200

    Improve firstsetup command initialization process
    - Updated socket file setup logic for better clarity and efficiency.
    - Integrated certificate generation for development environments.
    - Adjusted initialization commands, including creating the superuser and populating data.
    - Streamlined file and directory checks to avoid redundant operations.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- backend/ahs_core/management/commands/firstsetup.py
+++ backend/ahs_core/management/commands/firstsetup.py
@@ -1,6 +1,7 @@
 import stat
 import os.path
 import venv
+import subprocess
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -8,37 +9,32 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand
 
 PROJECT_DIR = settings.BASE_DIR
-
+SOCKET_DIR = PROJECT_DIR / 'docker' / '.sockets'
+PSQL_SOCKET_FILE = SOCKET_DIR / '.s.PGSQL.5432'
+PSQL_LOCK_FILE = SOCKET_DIR / '.s.PGSQL.5432.lock'
+REDIS_SOCKET_FILE = SOCKET_DIR / 'redis.sock'
 
 
 def _create_socket_files():
-    socket_dir = PROJECT_DIR / 'docker' / '.sockets'
-    if socket_dir.exists():
-        socket_dir.rmdir()
+    if not os.path.exists(SOCKET_DIR):
+        SOCKET_DIR.mkdir(0o770)
+    os.chmod(SOCKET_DIR, 0o770)
 
-    socket_dir.mkdir(0o770)
-    socket_dir.chmod(0o770)
+    if PSQL_LOCK_FILE.exists():
+        PSQL_LOCK_FILE.unlink()
 
-    psql_socket_file = socket_dir / '.s.PGSQL.5432'
-    psql_lock_file = socket_dir / '.s.PGSQL.5432.lock'
-    redis_socket_file = socket_dir / 'redis.sock'
-    socket_dir.chmod(0o770)
+    if PSQL_SOCKET_FILE.exists():
+        PSQL_SOCKET_FILE.unlink()
 
-    if psql_lock_file.exists():
-        psql_lock_file.unlink()
+    if REDIS_SOCKET_FILE.exists():
+        REDIS_SOCKET_FILE.unlink()
 
-    if psql_socket_file.exists():
-        psql_socket_file.unlink()
-
-    if redis_socket_file.exists():
-        redis_socket_file.unlink()
-
-    os.mknod(psql_socket_file, mode=stat.S_IFSOCK)
-    os.mknod(redis_socket_file, mode=stat.S_IFSOCK)
-    os.chown(redis_socket_file,1000, 1000)
-    os.chown(psql_socket_file,1000, 1000)
-    os.chmod(redis_socket_file, 0o770)
-    os.chmod(psql_socket_file, 0o770)
+    os.mknod(PSQL_SOCKET_FILE, mode=stat.S_IFSOCK)
+    os.mknod(REDIS_SOCKET_FILE, mode=stat.S_IFSOCK)
+    os.chown(REDIS_SOCKET_FILE,1000, 1000)
+    os.chown(PSQL_SOCKET_FILE,1000, 1000)
+    os.chmod(REDIS_SOCKET_FILE, 0o770)
+    os.chmod(PSQL_SOCKET_FILE, 0o770)
 
 
 def create_venv_directory():
@@ -61,8 +57,6 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        call_command('migrate')
-        call_command('loaddata', 'initial_data.json')
         env_param = options['env']
         if 'dev' in env_param.lower():
             dev_env = True
@@ -72,9 +66,18 @@ class Command(BaseCommand):
             prod_env = True
         else:
             raise ImproperlyConfigured('Invalid environment parameter')
-
         if dev_env:
             self.stdout.write(self.style.SUCCESS('Setting up AHS development server...'))
-            _create_socket_files()
+            if not os.path.exists(REDIS_SOCKET_FILE) or not os.path.exists(PSQL_SOCKET_FILE):
+                _create_socket_files()
+            if (not os.path.exists(PROJECT_DIR / '.certs/rootCA.pem') or
+                    not os.path.exists(PROJECT_DIR / '.certs/localhost.pem') or
+                    not os.path.exists(PROJECT_DIR / '.certs/localhost-key.pem')):
+                subprocess.run(['cd', f"{PROJECT_DIR / '.certs'}", '&&', './generate_ssl_certs_development.sh'])
         elif prod_env:
             self.stdout.write(self.style.SUCCESS('Setting up AHS production server...'))
+        #call_command('dockercompose', '--setup', '--start')
+        call_command('makemigrations')
+        call_command('migrate')
+        call_command('createsuperuser')
+        call_command('populate')
