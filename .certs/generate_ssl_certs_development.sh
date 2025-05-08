738337774fd0b902b3324e5333e00167a33b2cd1
dcx7c5 <dcxdevelopment@protonmail.com>
2025-05-08 22:36:57 +0200

    Enhance SSL certificate generation script
    - Added check to ensure 'mkcert' is installed before proceeding.
    - Improved OS detection logic and error handling for unsupported systems.
    - Replaced hardcoded cert renaming with dynamic logic using loops.
    - Enhanced output messages with formatted colors for better readability.
    - Included step to list newly generated '.pem' files for confirmation.
    
    Signed-off-by: dcx7c5 <dcxdevelopment@protonmail.com>

--- .certs/generate_ssl_certs_development.sh
+++ .certs/generate_ssl_certs_development.sh
@@ -16,16 +16,18 @@ install_pacman() {
     sudo pacman -S --noconfirm "$@"
 }
 
-echo -e "Installing ssl certificates for localhost and 127.0.0.1"
+echo -e "\e[1;32mInstalling ssl certificates for localhost and 127.0.0.1\e[0m"
 
-# Detect OS and call the correct function
-if grep -qi 'ID_LIKE=arch' /etc/os-release; then
-    install_pacman mkcert
-elif grep -Eqi 'ID_LIKE=(debian|ubuntu|linuxmint)' /etc/os-release; then
-    install_apt mkcert
-else
-    echo -e "\e[1;31mUnsupported OS.\e[0m"
-    exit 1
+if ! command -v mkcert &>/dev/null; then
+  # Detect OS and call the correct function
+  if grep -qi 'ID_LIKE=arch' /etc/os-release; then
+      install_pacman mkcert
+  elif grep -Eqi 'ID_LIKE=(debian|ubuntu|linuxmint)' /etc/os-release; then
+      install_apt mkcert
+  else
+      echo -e "\e[1;31mUnsupported OS.\e[0m"
+      exit 1
+  fi
 fi
 
 # Install the local CA in the system trust store.
@@ -34,12 +36,19 @@ mkcert -install -ecdsa
 # Create certificates for localhost
 mkcert -ecdsa localhost 127.0.0.1
 
+echo -e "\e[1;32mRenaming key and cert file...\e[0m"
+
+for f in localhost+*-key.pem; do
+  [ -f "$f" ] && mv "$f" "localhost-key.pem"
+done
+
+for f in localhost+*.pem; do
+  [[ "$f" != "localhost-key.pem" ]] && [ -f "$f" ] && mv "$f" "localhost.pem"
+done
+
 # Copy rootCA in `pwd`
 ROOT_CA="$(mkcert -CAROOT)/rootCA.pem"
 cp "$ROOT_CA" .
-
-# Rename certs
-mv "localhost+1.pem" "localhost.pem"
-mv "localhost+1-key.pem" "localhost-key.pem"
+find . -type f -name "*.pem" | sed 's|^\./||'
 
 echo -e "\e[1;32mInstalled certificates successfully\e[0m"
